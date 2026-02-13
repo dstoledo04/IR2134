57b510d5c3246fe9b5c5f962bf134e2f049d027e
DavidSanchez <al426662@uji.es>
2026-02-13 22:37:52 +0100

    fin

--- rmf_ws/build/rmf_demos_maps/CMakeFiles/generate_hotel_crowdsim.dir/cmake_clean.cmake
+++
@@ -1,9 +0,0 @@
-file(REMOVE_RECURSE
-  "CMakeFiles/generate_hotel_crowdsim"
-  "hotel_crowdsim"
-)
-
-# Per-language clean rules from dependency scanning.
-foreach(lang )
-  include(CMakeFiles/generate_hotel_crowdsim.dir/cmake_clean_${lang}.cmake OPTIONAL)
-endforeach()
