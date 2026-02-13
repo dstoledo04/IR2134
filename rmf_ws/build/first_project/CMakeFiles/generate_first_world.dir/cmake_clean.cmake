d2811224a418a82f326de23bf093bc99bdf19b2a
DavidSanchez <al426662@uji.es>
2026-02-13 19:42:46 +0100

    launch del first project completo

---
+++ rmf_ws/build/first_project/CMakeFiles/generate_first_world.dir/cmake_clean.cmake
@@ -0,0 +1,9 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/generate_first_world"
+  "maps/first_ign/first.world"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang )
+  include(CMakeFiles/generate_first_world.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
