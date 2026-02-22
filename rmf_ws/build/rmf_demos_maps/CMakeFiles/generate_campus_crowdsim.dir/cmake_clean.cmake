09b39052b76885307282b0a0e0b7a957803dd3b5
DavidSanchez004 <144883029+DavidSanchez004@users.noreply.github.com>
2026-02-22 18:25:57 +0100

    Delete rmf_ws/build directory

--- rmf_ws/build/rmf_demos_maps/CMakeFiles/generate_campus_crowdsim.dir/cmake_clean.cmake
+++
@@ -1,9 +0,0 @@
-file(REMOVE_RECURSE
-  "CMakeFiles/generate_campus_crowdsim"
-  "campus_crowdsim"
-)
-
-# Per-language clean rules from dependency scanning.
-foreach(lang )
-  include(CMakeFiles/generate_campus_crowdsim.dir/cmake_clean_${lang}.cmake OPTIONAL)
-endforeach()
--- rmf_ws/build/rmf_demos_maps/CMakeFiles/hotel.dir/cmake_clean.cmake
+++
@@ -1,8 +0,0 @@
-file(REMOVE_RECURSE
-  "CMakeFiles/hotel"
-)
-
-# Per-language clean rules from dependency scanning.
-foreach(lang )
-  include(CMakeFiles/hotel.dir/cmake_clean_${lang}.cmake OPTIONAL)
-endforeach()
